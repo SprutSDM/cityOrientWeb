32be07105bd1cc99e86fb79748bef673256a714d
SprutSDM <zakoylov@gmail.com>
2021-05-15 21:24:24 +0300

    Refactor leave api to viewset

--- backend/src/quests/api/endpoints.py
+++ backend/src/quests/api/endpoints.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:pk>/', views.QuestDetailView.as_view(), name='quest-detail'),
     path('<int:pk>/statistic', views.QuestStatisticDetailView.as_view(), name="quest-statistic"),
     path('<int:pk>/join', viewsets.QuestViewSet.as_view({'post': 'join'}), name="quest-join"),
-    path('<int:pk>/leave', views.QuestLeaveView.as_view(), name="quest-leave"),
+    path('<int:pk>/leave', viewsets.QuestViewSet.as_view({'post': 'leave'}), name="quest-leave"),
     path('task/<int:pk>/complete', views.TaskCompleteView.as_view(), name="complete-task"),
     path('task/<int:pk>/use_tip/<int:tip_number>', views.UseTipView.as_view(), name="use-tip")
 ]
--- backend/src/quests/api/viewsets.py
+++ backend/src/quests/api/viewsets.py
@@ -13,12 +13,17 @@ class QuestViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'])
     def join(self, request, pk=None):
         quest = self.get_object()
-
         tasks = Task.objects.filter(quest_id=quest.id)
         first_task = TeamStatistic.objects.filter(quest=quest.id).count() % tasks.count()
         team_statistic = TeamStatistic.objects.create(quest=quest, team=request.user, first_task=first_task)
         for task in tasks:
             TaskStatistic.objects.create(task=task, team_statistic=team_statistic)
+        serializer = self.get_serializer(quest)
+        return Response(serializer.data)
 
+    @action(detail=True, methods=['post'])
+    def leave(self, request, pk=None):
+        quest = self.get_object()
+        quest.teams_statistic.filter(team=self.request.user).delete()
         serializer = self.get_serializer(quest)
         return Response(serializer.data)
